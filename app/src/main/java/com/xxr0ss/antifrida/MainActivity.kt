35aa75ba6cbfd9147029aa2c0483fce1074a9b23
lang94 <15775691981@163.com>
2024-03-22 16:59:52 +0800

    pu

--- app/src/main/java/com/xxr0ss/antifrida/MainActivity.kt
+++ app/src/main/java/com/xxr0ss/antifrida/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.xxr0ss.antifrida
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.xxr0ss.antifrida.databinding.ActivityMainBinding
 import com.xxr0ss.antifrida.utils.AntiFrida2
 import com.xxr0ss.antifrida.utils.AntiFridaUtil
 import com.xxr0ss.antifrida.utils.ReadVia
 import com.xxr0ss.antifrida.utils.SuperUser
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +36,7 @@ class MainActivity : AppCompatActivity() {
     // put possible frida module names here
     private val frida_module_blocklist = listOf("frida-agent", "frida-gadget")
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -96,6 +109,15 @@ class MainActivity : AppCompatActivity() {
             val useMySyscalls = binding.switchUseMySyscalls.isChecked
             // not all signatures here exist in the latest frida modules
             // if you find any signature that works, just put it here
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+            if (!Environment.isExternalStorageManager()) {
+                val intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+                startActivity(intent)
+            }
+            var txt = read_file("/sdcard/Download/files_fd/","frida_script.js")
+            if (txt != null) {
+                Log.d("frida-OOOK",txt)
+            };
             val blockList = listOf("frida")
             var detected = false;
             blockList.forEach {
@@ -140,5 +162,21 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    fun read_file(path: String?, file: String?): String? {
+        try {
+            val fs = FileInputStream(File(path, file))
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            val buffer = ByteArray(4096)
+            var len = 0
+            while (fs.read(buffer).also { len = it } != -1) {
+                byteArrayOutputStream.write(buffer, 0, len)
+            }
+            return byteArrayOutputStream.toString()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return ""
     }
 }
